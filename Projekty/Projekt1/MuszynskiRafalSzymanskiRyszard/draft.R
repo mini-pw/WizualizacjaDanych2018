00ebeb17fb947bfe60844a059a4172a0adf2c017
szymanskir <ryszard.szymanski@outlook.com>
2019-03-17 22:48:16 +0100

    Better drafts

--- Projekty/Projekt1/MuszynskiRafalSzymanskiRyszard/draft.R
+++ Projekty/Projekt1/MuszynskiRafalSzymanskiRyszard/draft.R
@@ -1,23 +1,23 @@
+library(anytime)
 library(dplyr)
 library(ggplot2)
+library(jsonlite)
 library(lubridate)
 library(readr)
+library(scales)
 
+
+#' Step data
 activity <- read_csv("~/mi-dane/ACTIVITY/ACTIVITY_1551727501568.csv")
 
-#' Steps per day of week
 activity %>% 
-  mutate(weekday = wday(date)) %>% 
-  ggplot(aes(x = weekday, y = steps, fill = weekday)) + 
-  geom_bar(stat = "summary", fun.y = "mean") 
+  mutate(
+    month = month(date, abbr = T),
+    quality = ifelse(steps > 8000, "Yes", "No")) %>% 
+  ggplot(aes(x = date, y = steps, fill = quality)) + 
+  geom_bar(stat = "summary", fun.y = "identity") 
 
-#' Steps per month
-activity %>% 
-  mutate(month = month(date)) %>% 
-  ggplot(aes(x = month, y = steps, fill = month)) + 
-  geom_bar(stat = "summary", fun.y = "mean") 
-
-
+#' Sleep data
 sleep <- read_csv("~/mi-dane/SLEEP/SLEEP_1551727501867.csv")
 
 sleep_data <- sleep %>% 
@@ -60,33 +60,33 @@ sleep %>%
   geom_line()
 
 
+sleep %>% filter(deepSleepTime > 0 & shallowSleepTime > 0) %>%
+  ggplot(aes(x = deepSleepTime + shallowSleepTime)) + 
+  geom_density()
+
+
 ## messenger data
-
-library(jsonlite)
-library(dplyr)
-library(anytime)
-
-files <- list.files(path="~/messages/inbox", pattern='message.json', full.names=TRUE, recursive=TRUE)
+files <- list.files(path="~/Downloads/facebook-rysiekszymanski/messages/inbox", pattern='message.json', full.names=TRUE, recursive=TRUE)
 
 data <- lapply(files, function(filename) {
   fromJSON(filename)$messages %>%
-    filter(sender_name == "Rafa\u00c5\u0082 Muszy\u00c5\u0084ski") %>%
-    mutate(length = nchar(content), date = anydate(timestamp_ms/1000)) %>%
-    select(length, date)
-})
-
-data <- lapply(files, function(filename) {
-  fromJSON(filename)$messages %>%
-    filter(sender_name == "Rafa\u00c5\u0082 Muszy\u00c5\u0084ski") %>%
-    mutate(date = anydate(timestamp_ms/1000)) %>%
-    group_by(date) %>%
-    summarise(msgCount =  count())
+    mutate(time = ymd_hms(anytime(timestamp_ms/1000)), "%H:%M:%S") %>% 
+    select(time)
 })
 
 df <- do.call(rbind, data)
 
-df %>%
-  group_by(date) %>%
-  summarise(lengthSum = sum(length))
-
-
+#'
+df %>% 
+  mutate(time = time %>% 
+           as.POSIXct(format = "%H:%M:%S") %>%
+           strftime(format = "%H:00:00") %>% 
+           as.POSIXct(format = "%H:%M:%S")
+         ) %>% 
+  group_by(time) %>% 
+  summarize(msg_count = n()) %>% 
+  ggplot(aes(x = time, y = 0, fill = msg_count)) +
+  geom_tile() + 
+  scale_fill_gradient(low = "blue", high = "red") +
+  scale_x_datetime(labels = date_format("%H"),
+                   date_breaks = "1 hours")
