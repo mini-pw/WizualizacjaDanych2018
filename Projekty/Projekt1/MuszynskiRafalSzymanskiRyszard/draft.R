e4b31c40739d29dc6c1d95ad86aab79826b667a8
ramusz1 <ramusz@wp.pl>
2019-03-19 00:36:53 +0100

    sleep graph

--- Projekty/Projekt1/MuszynskiRafalSzymanskiRyszard/draft.R
+++ Projekty/Projekt1/MuszynskiRafalSzymanskiRyszard/draft.R
@@ -15,7 +15,7 @@ activity %>%
     month = month(date, abbr = T),
     quality = ifelse(steps > 8000, "Yes", "No")) %>% 
   ggplot(aes(x = date, y = steps, fill = quality)) + 
-  geom_bar(stat = "summary", fun.y = "identity") 
+  geom_bar(stat = "summary", fun.y = "identity")
 
 #' Sleep data
 sleep <- read_csv("~/mi-dane/SLEEP/SLEEP_1551727501867.csv")
@@ -59,14 +59,49 @@ sleep %>%
   ggplot(aes(x = date, y = total_sleep_time)) + 
   geom_line()
 
+# sleep density 
+# src: https://www.ofeminin.pl/fitness-i-zdrowie/dolegliwoscichoroby/ile-godzin-snu-potrzebujemy-w-zaleznosci-od-wieku-tabela/g8flrtx
+# < 6 - bad 
+# 6-7 to little but ok
+# 7 - 9 bueno
+# 9 - 11 too much but ok
+# > 11 too much
+sleepBreaks <- c(6,7,9,11)
 
-sleep %>% filter(deepSleepTime > 0 & shallowSleepTime > 0) %>%
-  ggplot(aes(x = deepSleepTime + shallowSleepTime)) + 
-  geom_density()
+sleepDensity <-
+  sleep %>% filter(deepSleepTime > 0 & shallowSleepTime > 0) %>%
+  mutate(total = (deepSleepTime + shallowSleepTime) / 60) %>%
+  pull(total) %>%
+  density()
 
+data.frame(x=sleepDensity$x, y=sleepDensity$y) %>%
+  mutate(quality = factor(findInterval(x, sleepBreaks))) %>%
+  ggplot(aes(x,y)) +
+  geom_line() +
+  geom_ribbon(aes(ymin=0, ymax=y, fill=quality), color = NA) +
+  scale_x_continuous(breaks=sleepBreaks, name = 'długość snu') +
+  scale_fill_manual( values = c('red','orange','green','orange','red')) +
+  scale_y_continuous(labels = NULL) +
+  theme(axis.title.y=element_blank(),
+        axis.text.y=element_blank(),
+        axis.ticks.y=element_blank(), 
+        panel.background = element_rect(fill = "white",
+                                        colour = "white",
+                                        size = 0.5, linetype = "solid"),
+        panel.grid.major = element_blank(),  
+        panel.grid.minor = element_blank())
+
+
+# compute cumulative distribution function
+f <- approxfun(sleepDensity$x, sleepDensity$y, yleft=0, yright=0)
+sleepCDF <-function(x){integrate(f, -Inf, x)}
+fractions <- sapply(sleepBreaks, sleepCDF)['value', ]
+
+# some cool usage of cdf
+percentageOfReallyBadSleep <- fractions[[1]] + (1 - fractions[[4]])
 
 ## messenger data
-files <- list.files(path="~/Downloads/facebook-rysiekszymanski/messages/inbox", pattern='message.json', full.names=TRUE, recursive=TRUE)
+files <- list.files(path="~/Downloads/facebook-data/messages/inbox", pattern='message.json', full.names=TRUE, recursive=TRUE)
 
 data <- lapply(files, function(filename) {
   fromJSON(filename)$messages %>%
