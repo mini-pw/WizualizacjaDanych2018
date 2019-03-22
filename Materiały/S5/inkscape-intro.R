119ebf5f92d1ecb153bb8300d4257f3452b73327
michbur <michalburdukiewicz@gmail.com>
2019-03-22 16:46:39 +0100

    fixed latex2exp

--- Materiały/S5/inkscape-intro.R
+++ Materiały/S5/inkscape-intro.R
@@ -80,7 +80,7 @@ library(latex2exp)
 p1 <- ggplot(data = countries, aes(x = continent, y = death.rate, color = continent)) +
   geom_boxplot() +
   scale_x_discrete("ĄĘŻŹĆ") #+
-  #scale_y_continuous(TeX("$\\frac{\\alpha}{\\beta \\times \\log 10}$"))
+  scale_y_continuous(TeX("$\\frac{\\alpha}{\\beta \\times \\log 10}$"))
 
 set.seed(1410)
 p2 <- ggplot(data = countries, aes(x = continent, y = death.rate, color = continent)) +
@@ -89,7 +89,6 @@ p2 <- ggplot(data = countries, aes(x = continent, y = death.rate, color = contin
 p3 <- ggplot(data = countries, aes(x = continent, fill = continent)) +
   geom_bar()
 
-loadfonts(device = "svg")
 p <- ((p1 + p2) / p3) * theme_bw() * 
   theme(legend.background = element_rect(fill = "green"),
         text=element_text(family="Times New Roman", face = "bold"))
