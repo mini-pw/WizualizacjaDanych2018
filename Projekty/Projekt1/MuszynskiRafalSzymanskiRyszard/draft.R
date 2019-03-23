8ff19198f94b0b3718ec9fce9a7cb96b94598fea
szymanskir <ryszard.szymanski@outlook.com>
2019-03-23 17:18:52 +0100

    * Aggregate steps per week * Add export to svg

--- Projekty/Projekt1/MuszynskiRafalSzymanskiRyszard/draft.R
+++ Projekty/Projekt1/MuszynskiRafalSzymanskiRyszard/draft.R
@@ -4,61 +4,38 @@ library(ggplot2)
 library(jsonlite)
 library(lubridate)
 library(readr)
+library(gridSVG)
 library(scales)
 library(viridis)
 
 
-#' Step data
+#' Steps data
 activity <- read_csv("~/mi-dane/ACTIVITY/ACTIVITY_1551727501568.csv")
 
-activity %>% 
+activity_by_week <- activity %>% 
+  group_by(week = as.Date(cut(date, "week"))) %>% 
+  summarise(steps = mean(steps)) %>% 
   mutate(
-    month = month(date, abbr = T),
-    quality = ifelse(steps > 8000, "Yes", "No")) %>% 
-  ggplot(aes(x = date, y = steps, fill = quality)) + 
-  geom_bar(stat = "summary", fun.y = "identity")
+    Quality = ifelse(steps < 8000, "Bad", "Good") %>% 
+      factor(levels = c("Good", "Bad"))
+  )
+
+activity_plot <- ggplot(data = activity_by_week,
+       aes(x = week, y = steps, fill = Quality)) +
+  geom_bar(stat = "summary", fun.y = "identity") +
+  scale_x_date() +
+  scale_fill_viridis_d(begin = 0.8, end = 0.1) +
+  xlab("Date") +
+  ylab("Average number of steps") +
+  theme_minimal(base_size = 22)
+
+
+svg("~/activity_plot.svg", bg = "transparent", height = 4.5, width = 7)
+activity_plot
+dev.off()
 
 #' Sleep data
 sleep <- read_csv("~/mi-dane/SLEEP/SLEEP_1551727501867.csv")
-
-sleep_data <- sleep %>% 
-  mutate(
-    weekday = weekdays(date),
-    total_sleep_time = deepSleepTime + shallowSleepTime,
-    bed_time = factor(start %>% anytime() %>% hour()),
-    wakeup_time = factor(stop %>% anytime() %>% hour()),
-  ) %>% 
-  filter(total_sleep_time > 0) %>% 
-  mutate(
-    deep_sleep_pct = deepSleepTime / total_sleep_time
-  )
-
-#' sleep per day of the week
-ggplot(sleep_data, aes(x = weekday, y = total_sleep_time, fill = weekday)) + 
-  geom_bar(stat = "summary", fun.y = "mean") +
-  coord_flip()
-
-#' Percentage of deep sleep
-ggplot(sleep_data, aes(x = weekday, y = deep_sleep_pct, fill = weekday)) + 
-  geom_bar(stat = "summary", fun.y = "mean") +
-  coord_flip()
-
-#' At what time do I go to sleep usually?
-ggplot(sleep_data, aes(x = bed_time)) + 
-  geom_bar(stat = "count") +
-  coord_flip()
-
-#' At What time do I wake up usually?
-ggplot(sleep_data, aes(x = wakeup_time)) + 
-  geom_bar(stat = "count") +
-  coord_flip()
-
-#' sleep per weeks
-sleep %>%
-  mutate(total_sleep_time = deepSleepTime + shallowSleepTime) %>% 
-  filter(total_sleep_time > 300) %>% 
-  ggplot(aes(x = date, y = total_sleep_time)) + 
-  geom_line()
 
 # sleep density 
 # src: https://www.ofeminin.pl/fitness-i-zdrowie/dolegliwoscichoroby/ile-godzin-snu-potrzebujemy-w-zaleznosci-od-wieku-tabela/g8flrtx
@@ -75,13 +52,15 @@ sleepDensity <-
   pull(total) %>%
   density()
 
-data.frame(x=sleepDensity$x, y=sleepDensity$y) %>%
+scale_fill_manual(values = c('red','orange','green','orange','red'))
+
+sleep_plot <- data.frame(x=sleepDensity$x, y=sleepDensity$y) %>%
   mutate(quality = factor(findInterval(x, sleepBreaks))) %>%
   ggplot(aes(x,y)) +
   geom_line() +
   geom_ribbon(aes(ymin=0, ymax=y, fill=quality), color = NA) +
-  scale_x_continuous(breaks=sleepBreaks, name = 'długość snu') +
-  scale_fill_manual( values = c('red','orange','green','orange','red')) +
+  scale_x_continuous(breaks=sleepBreaks, name = 'Sleep length') +
+  scale_fill_manual(values = c('red','orange','green','orange','red')) +
   scale_y_continuous(labels = NULL) +
   theme(axis.title.y=element_blank(),
         axis.text.y=element_blank(),
@@ -90,8 +69,12 @@ data.frame(x=sleepDensity$x, y=sleepDensity$y) %>%
                                         colour = "white",
                                         size = 0.5, linetype = "solid"),
         panel.grid.major = element_blank(),  
-        panel.grid.minor = element_blank())
+        panel.grid.minor = element_blank()) +
+  theme_minimal(base_size = 22)
 
+svg("~/sleep_plot.svg", bg = "transparent", height = 4.5, width = 7)
+sleep_plot
+dev.off()
 
 # compute cumulative distribution function
 f <- approxfun(sleepDensity$x, sleepDensity$y, yleft=0, yright=0)
@@ -104,34 +87,35 @@ percentageOfReallyBadSleep <- fractions[[1]] + (1 - fractions[[4]])
 ## messenger data
 files <- list.files(path="~/Downloads/facebook-data/messages/inbox", pattern='message.json', full.names=TRUE, recursive=TRUE)
 
-data <- lapply(files, function(filename) {
+df <- lapply(files, function(filename) {
   fromJSON(filename)$messages %>%
     mutate(time = ymd_hms(anytime(timestamp_ms/1000)), "%H:%M:%S") %>% 
     select(time)
-})
-
-df <- do.call(rbind, data)
+}) %>% bind_rows()
 
 #'
 fb_msg_cycle <- df %>% 
   mutate(time = time %>% 
-           as.POSIXct(format = "%H:%M:%S") %>%
-           strftime(format = "%H:%M:00") %>% 
+           strftime(format = "%H:00:00") %>% 
            as.POSIXct(format = "%H:%M:%S")
   ) %>% 
   group_by(time) %>% 
   summarize(msg_count = n()) 
 
-fb_msg_cycle %>% 
+fb_activity_plot <- fb_msg_cycle %>% 
   ggplot(aes(x = time, y = 0, fill = msg_count)) +
   geom_tile() + 
   scale_fill_viridis() +
   scale_x_datetime(labels = date_format("%H"),
                    date_breaks = "1 hours") +
-  theme_minimal() +
+  theme_minimal(base_size = 22) +
   theme(axis.text.y = element_blank(),
         axis.title.x = element_blank(),
-        axis.title.y = element_blank()) +
+        axis.title.y = element_blank(),
+        legend.position = "none") +
   coord_polar(start = -0.38) +
-  labs(fill = "Liczba wiadomości")
+  labs(fill = "Amount of messages")
   
+svg("~/fb_plot.svg", bg = "transparent", height = 4.5, width = 7)
+fb_activity_plot
+dev.off()
