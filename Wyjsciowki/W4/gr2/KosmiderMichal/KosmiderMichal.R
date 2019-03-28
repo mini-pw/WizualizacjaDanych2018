98d623c01a041be7b45dc89b5cb4747da4ccdc0b
279160 kosmider michal <kosmiderm@p30410.mini.pw.edu.pl>
2019-03-28 13:35:11 +0100

    Wyjsciowka 4 -- Michal Kosmider -- Added labels to facet grid.

--- Wyjsciowki/W4/gr2/KosmiderMichal/KosmiderMichal.R
+++ Wyjsciowki/W4/gr2/KosmiderMichal/KosmiderMichal.R
@@ -1,6 +1,7 @@
 library(ggplot2)
 
 ggplot(ChickWeight, aes(x = Time, y = weight)) + 
-  geom_point(aes(col = Chick)) + facet_grid(Diet ~ .) + 
-  stat_smooth() + 
+  geom_point(aes(col = Chick)) + 
+  facet_grid(Diet ~ ., labeller = label_both) + 
+  stat_smooth(se = FALSE) + 
   theme(legend.position = 'none')
