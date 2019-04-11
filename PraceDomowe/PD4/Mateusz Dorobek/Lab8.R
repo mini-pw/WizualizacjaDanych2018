853c1464520f6090defaf159ba0cc56196a2fb6f
SaxMan96 <mateuszdorobek@gmail.com>
2019-04-11 23:16:34 +0200

    Praca Domowa 4

---
+++ PraceDomowe/PD4/Mateusz Dorobek/Lab8.R
@@ -0,0 +1,7 @@
+library(MASS)
+library("rsconnect")
+rsconnect::setAccountInfo(name='saxman96',
+                          token='2A345F6B4761853F0C1A6AB87FDCC4DC',
+                          secret='yiCphWO4GWj3YhKnAQJxfFs5jtp/k5vct2qvNwxr')
+rsconnect::deployApp("F:/MiNI IAD/Sem 1/Wizualizacja Danych/Cwiczenia/Cw6-shiny")
+
