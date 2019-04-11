016db5c9f83fbe721fc01775de44bd8c42af4820
michbur <michalburdukiewicz@gmail.com>
2019-04-11 12:17:26 +0200

    path fixed

--- Materiały/S8/app-deployment.R
+++ Materiały/S8/app-deployment.R
@@ -1,3 +1,3 @@
 # install.packages("rsconnect")
 # token: https://www.shinyapps.io/
-rsconnect::deployApp(appDir = "Materiały/S7/app.R")
+rsconnect::deployApp(appDir = "Materiały/S7/SimpleShinyApp/sapp.R")
