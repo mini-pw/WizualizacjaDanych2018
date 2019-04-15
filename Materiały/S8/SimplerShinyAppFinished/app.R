05ca72c3884128ef5e25e93428d34a2bf4c1028e
michbur <michalburdukiewicz@gmail.com>
2019-04-15 13:42:04 +0200

    awful hack added

--- Materiały/S8/SimplerShinyAppFinished/app.R
+++ Materiały/S8/SimplerShinyAppFinished/app.R
@@ -21,32 +21,32 @@ server <- function(input, output) {
     selected_countires[["selected"]] <- c(selected_countires[["selected"]], 
                                           nearPoints(countries, input[["country_click"]], 
                                                      maxpoints = 1)[["country"]])
-    
-    countries_duplicates <- table(country = selected_countires[["selected"]]) %>% 
-      as.data.frame(stringsAsFactors = FALSE) %>% 
-      filter(Freq > 1) %>% 
-      pull(country)
-    
-    selected_countires[["selected"]] <- setdiff(selected_countires[["selected"]], countries_duplicates)
-    
+    if(length(selected_countires[["selected"]]) > 0) {
+      countries_duplicates <- table(country = selected_countires[["selected"]]) %>% 
+        as.data.frame(stringsAsFactors = FALSE) %>% 
+        filter(Freq > 1) %>% 
+        pull(country)
+      
+      selected_countires[["selected"]] <- setdiff(selected_countires[["selected"]], countries_duplicates)
+    }
   })
-    
-    continent_colors <- c(Asia = "red", Europe = "green", Africa = "orange", Americas = "black", 
-                          Oceania = "blue")
-    
-    output[["countries_plot"]] <- renderPlot({
-      mutate(countries, selected = country %in% selected_countires[["selected"]]) %>% 
-        ggplot(aes(x = birth.rate, y = death.rate, color = continent, size = selected)) +
-        geom_point() +
-        scale_color_manual(values = continent_colors) +
-        scale_size_manual(values = c(2, 10)) +
-        theme_bw()
-    })
-    
-    output[["plot_value"]] <- renderPrint({
-      dput(selected_countires[["selected"]])
-    })
-    
+  
+  continent_colors <- c(Asia = "red", Europe = "green", Africa = "orange", Americas = "black", 
+                        Oceania = "blue")
+  
+  output[["countries_plot"]] <- renderPlot({
+    mutate(countries, selected = country %in% selected_countires[["selected"]]) %>% 
+      ggplot(aes(x = birth.rate, y = death.rate, color = continent, size = selected)) +
+      geom_point() +
+      scale_color_manual(values = continent_colors) +
+      scale_size_manual(values = c(2, 10)) +
+      theme_bw()
+  })
+  
+  output[["plot_value"]] <- renderPrint({
+    dput(selected_countires[["selected"]])
+  })
+  
 }
 
 shinyApp(ui = ui, server = server)
