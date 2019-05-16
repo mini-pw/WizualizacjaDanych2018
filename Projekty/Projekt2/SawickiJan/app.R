3adec3b2d587f3e62898cb5dc5673ca741e6f5c9
JanSawicki <jansawickisawicki@gmail.com>
2019-05-16 20:48:46 +0200

    Added the initial version of the second case - bar plot with bad axis

--- Projekty/Projekt2/SawickiJan/app.R
+++ Projekty/Projekt2/SawickiJan/app.R
@@ -9,50 +9,86 @@ ui <- dashboardPage(
   dashboardHeader(title = "WiZŁAlizacja"),
   dashboardSidebar(sidebarMenu(
     id = "tabs",
+    menuItem("Pie plot",
+             tabName = "pie_plot",
+             icon = icon("dashboard")),
     menuItem(
-      "Dashboard",
-      tabName = "dashboard",
+      "Bar plot (bad axis)",
+      tabName = "bar_plot_bad_axis",
       icon = icon("dashboard")
     )
   )),
   dashboardBody(tabItems(
-    tabItem(
-      "dashboard",
-      fluidRow(
-        box(
-          title = "Bad plot",
-          status = "primary",
-          solidHeader = T,
-          collapsible = F,
-          plotOutput("pie_plot"),
-          width = 3
-        ),
-        box(
-          title = "Your plot",
-          status = "primary",
-          solidHeader = TRUE,
-          collapsible = F,
-          textInput(inputId = "pie_plot_data",
-                    label = "Guess the proportion of category A (%)"),
-          plotOutput("pie_plot_guessed"),
-          width = 3
-        ),
-        box(
-          title = "Good plot",
-          status = "primary",
-          solidHeader = T,
-          collapsible = T,
-          collapsed = T,
-          verbatimTextOutput("pie_plot_original_values"),
-          plotOutput("pie_plot_good"),
-          width = 3
-        )
-      )
-    )
+    tabItem("pie_plot",
+            fluidRow(
+              box(
+                title = "Bad plot",
+                status = "primary",
+                solidHeader = T,
+                collapsible = F,
+                plotOutput("pie_plot_bad"),
+                width = 3
+              ),
+              box(
+                title = "Your plot",
+                status = "primary",
+                solidHeader = TRUE,
+                collapsible = F,
+                textInput(inputId = "pie_plot_data",
+                          label = "Guess the proportion of category A (%)"),
+                plotOutput("pie_plot_guessed"),
+                width = 3
+              ),
+              box(
+                title = "Good plot",
+                status = "primary",
+                solidHeader = T,
+                collapsible = T,
+                collapsed = T,
+                verbatimTextOutput("pie_plot_original_values"),
+                plotOutput("pie_plot_good"),
+                width = 3
+              )
+            )),
+    tabItem("bar_plot_bad_axis",
+            fluidRow(
+              box(
+                title = "Bad plot",
+                status = "primary",
+                solidHeader = T,
+                collapsible = F,
+                plotOutput("bar_plot_bad_axis_bad"),
+                width = 3
+              ),
+              box(
+                title = "Your plot",
+                status = "primary",
+                solidHeader = TRUE,
+                collapsible = F,
+                textInput(inputId = "bar_plot_bad_axis_data1",
+                          label = "Guess the value of category A", value = "100"),
+                textInput(inputId = "bar_plot_bad_axis_data2",
+                          label = "Guess the value of category B", value = "50"),
+                plotOutput("bar_plot_bad_axis_guessed"),
+                width = 3
+              ),
+              box(
+                title = "Good plot",
+                status = "primary",
+                solidHeader = T,
+                collapsible = T,
+                collapsed = T,
+                verbatimTextOutput("bar_plot_bad_axis_original_values"),
+                plotOutput("bar_plot_bad_axis_good"),
+                width = 3
+              )
+            ))
   ))
 )
 
 server <- function(input, output) {
+  # PIE PLOT
+  # ------------------------------------------------------------------------------------------------------------------------------------------
   data_pie_plot <- reactive({
     data.frame(
       Category = c("A", "B"),
@@ -61,7 +97,7 @@ server <- function(input, output) {
     )
   })
   
-  output[["pie_plot"]] <- renderPlot({
+  output[["pie_plot_bad"]] <- renderPlot({
     ggplot(data_pie_plot(), aes(x = "", y = Value, fill = Category)) +
       geom_bar(stat = "identity") +
       ylim(c(0, 100)) +
@@ -73,7 +109,7 @@ server <- function(input, output) {
   output[["pie_plot_good"]] <- renderPlot({
     ggplot(data_pie_plot(), aes(x = Category, y = Value, fill = Category)) +
       geom_bar(stat = "identity") +
-      geom_text(aes(label = Value), vjust = 0) +
+      geom_text(aes(label = Value), vjust = -0.5) +
       ylim(c(0, 100)) +
       ggtitle("Good") +
       theme_minimal() +
@@ -82,7 +118,11 @@ server <- function(input, output) {
   
   output[["pie_plot_guessed"]] <- renderPlot({
     ggplot(data_pie_plot(),
-           aes(x = "", y = Value_Guessed, fill = Category)) +
+           aes(
+             x = "",
+             y = data_pie_plot()$Value_Guessed,
+             fill = Category
+           )) +
       geom_bar(stat = "identity") +
       ylim(c(0, 100)) +
       coord_polar("y", start = 0) +
@@ -90,32 +130,45 @@ server <- function(input, output) {
       theme_minimal()
   })
   
-  output[["pie_plot_original_values"]] <- renderText({
-    data_pie_plot()$Values
+  # BAR PLOT WITH BAD AXIS
+  # ------------------------------------------------------------------------------------------------------------------------------------------
+  
+  data_bar_plot_bad_axis <- reactive({
+    data.frame(
+      Category = c("A", "B"),
+      Value = c(75, 76),
+      Value_Guessed = c(as.numeric(input[["bar_plot_bad_axis_data1"]]), as.numeric(input[["bar_plot_bad_axis_data2"]]))
+    )
   })
   
-  values <- reactiveValues()
-  values$show_pie_plot_good <- F
-  
-  output$show_pie_plot_good <- reactive({
-    return(values$show_pie_plot_good)
+  output[["bar_plot_bad_axis_bad"]] <- renderPlot({
+    ggplot(data_bar_plot_bad_axis(),
+           aes(x = Category, y = Value, fill = Category)) +
+      geom_bar(stat = "identity") +
+      coord_cartesian(ylim = c(74, 77))  +
+      ggtitle("Original") +
+      theme_minimal() +
+      theme(axis.text.y = element_blank())
   })
   
-  observeEvent(input$pie_plot_button, {
-    toggle('pie_plot_values')
-    output$pie_plot_original_values <-
-      renderText({
-        paste0(
-          data_pie_plot()$Category[1],
-          ": ",
-          data_pie_plot()$Value[1],
-          " ",
-          data_pie_plot()$Category[2],
-          ": ",
-          data_pie_plot()$Value[2]
-        )
-      })
-    values$show_pie_plot_good = T
+  output[["bar_plot_bad_axis_good"]] <- renderPlot({
+    ggplot(data_bar_plot_bad_axis(),
+           aes(x = Category, y = Value, fill = Category)) +
+      geom_bar(stat = "identity") +
+      geom_text(aes(label = Value), vjust = -0.5) +
+      ylim(c(0, 100)) +
+      ggtitle("Good") +
+      theme_minimal() +
+      theme(legend.position = "none")
+  })
+  
+  output[["bar_plot_bad_axis_guessed"]] <- renderPlot({
+    ggplot(data_bar_plot_bad_axis(),
+           aes(x = "", y = Value_Guessed, fill = Category)) +
+      geom_bar(stat = "identity") +
+      coord_cartesian(ylim = c(data_bar_plot_bad_axis()$Value_Guessed[0], data_bar_plot_bad_axis()$Value_Guessed[1])) +
+      ggtitle("Your guess") +
+      theme_minimal()
   })
 }
 
