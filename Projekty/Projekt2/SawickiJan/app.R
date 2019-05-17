2790c48acfa7d29c9e3da3c8d251c6839b612612
JanSawicki <jansawickisawicki@gmail.com>
2019-05-17 22:39:09 +0200

    Added bar chart visualization with too much graphics

--- Projekty/Projekt2/SawickiJan/app.R
+++ Projekty/Projekt2/SawickiJan/app.R
@@ -3,6 +3,11 @@ library(shinydashboard)
 library(reshape2)
 library(dplyr)
 library(ggplot2)
+library(png)
+library(grid)
+library(RCurl)
+library(cowplot)
+library(magick)
 
 ui <- dashboardPage(
   skin = "blue",
@@ -15,6 +20,11 @@ ui <- dashboardPage(
     menuItem(
       "Bar plot (bad axis)",
       tabName = "bar_plot_bad_axis",
+      icon = icon("dashboard")
+    ),
+    menuItem(
+      "Bar plot (too much graphics)",
+      tabName = "bar_plot_too_much_graphics",
       icon = icon("dashboard")
     )
   )),
@@ -65,10 +75,16 @@ ui <- dashboardPage(
                 status = "primary",
                 solidHeader = TRUE,
                 collapsible = F,
-                textInput(inputId = "bar_plot_bad_axis_data1",
-                          label = "Guess the value of category A", value = "100"),
-                textInput(inputId = "bar_plot_bad_axis_data2",
-                          label = "Guess the value of category B", value = "50"),
+                textInput(
+                  inputId = "bar_plot_bad_axis_data1",
+                  label = "Guess min on Y scale",
+                  value = "-1000"
+                ),
+                textInput(
+                  inputId = "bar_plot_bad_axis_data2",
+                  label = "Guess max on Y scale",
+                  value = "1000"
+                ),
                 plotOutput("bar_plot_bad_axis_guessed"),
                 width = 3
               ),
@@ -80,6 +96,37 @@ ui <- dashboardPage(
                 collapsed = T,
                 verbatimTextOutput("bar_plot_bad_axis_original_values"),
                 plotOutput("bar_plot_bad_axis_good"),
+                width = 3
+              )
+            )),
+    tabItem("bar_plot_too_much_graphics",
+            fluidRow(
+              box(
+                title = "Bad plot",
+                status = "primary",
+                solidHeader = T,
+                collapsible = F,
+                plotOutput("bar_plot_too_much_graphics_bad"),
+                width = 3
+              ),
+              box(
+                title = "Your plot",
+                status = "primary",
+                solidHeader = TRUE,
+                collapsible = F,
+                textInput(inputId = "bar_plot_too_much_graphics_data",
+                          label = "Guess the proportion of category A (%)"),
+                plotOutput("bar_plot_too_much_graphics_guessed"),
+                width = 3
+              ),
+              box(
+                title = "Good plot",
+                status = "primary",
+                solidHeader = T,
+                collapsible = T,
+                collapsed = T,
+                verbatimTextOutput("bar_plot_too_much_graphics_original_values"),
+                plotOutput("bar_plot_too_much_graphics_good"),
                 width = 3
               )
             ))
@@ -164,11 +211,70 @@ server <- function(input, output) {
   
   output[["bar_plot_bad_axis_guessed"]] <- renderPlot({
     ggplot(data_bar_plot_bad_axis(),
-           aes(x = "", y = Value_Guessed, fill = Category)) +
+           aes(x = Category, y = Value, fill = Category)) +
       geom_bar(stat = "identity") +
-      coord_cartesian(ylim = c(data_bar_plot_bad_axis()$Value_Guessed[0], data_bar_plot_bad_axis()$Value_Guessed[1])) +
+      coord_cartesian(
+        ylim = c(
+          data_bar_plot_bad_axis()$Value_Guessed[1],
+          data_bar_plot_bad_axis()$Value_Guessed[2]
+        )
+      ) +
       ggtitle("Your guess") +
       theme_minimal()
+  })
+  
+  # BAR PLOT WITH TOO MUCH GRAPHICS
+  # ------------------------------------------------------------------------------------------------------------------------------------------
+  
+  dogeImage = "http://www.stickpng.com/assets/images/5845e69dfb0b0755fa99d7ef.png"
+  
+  data_bar_plot_too_much_graphics <- reactive({
+    data.frame(
+      Category = c("A", "B"),
+      Value = c(23, 77),
+      Value_Guessed = c(as.numeric(input[["bar_plot_too_much_graphics_data"]]), 100 - as.numeric(input[["bar_plot_too_much_graphics_data"]]))
+    )
+  })
+  
+  output[["bar_plot_too_much_graphics_bad"]] <- renderPlot({
+    ggdraw() +
+      draw_plot(
+        ggplot(
+          data_bar_plot_too_much_graphics(),
+          aes(x = Category, y = Value, fill = Category)
+        ) +
+          geom_bar(stat = "identity") +
+          ggtitle("Original") +
+          theme_minimal()
+      ) +
+      draw_image(dogeImage)
+  })
+  
+  output[["bar_plot_too_much_graphics_good"]] <- renderPlot({
+    ggplot(data_bar_plot_too_much_graphics(),
+           aes(x = Category, y = Value, fill = Category)) +
+      geom_bar(stat = "identity") +
+      geom_text(aes(label = Value), vjust = -0.5) +
+      ylim(c(0, 100)) +
+      ggtitle("Good") +
+      theme_minimal() +
+      theme(legend.position = "none")
+  })
+  
+  output[["bar_plot_too_much_graphics_guessed"]] <- renderPlot({
+    ggdraw() +
+      draw_plot(
+        ggplot(
+          data_bar_plot_too_much_graphics(),
+          aes(x = Category,
+              y = Value_Guessed,
+              fill = Category)
+        ) +
+          geom_bar(stat = "identity") +
+          ggtitle("Your guess") +
+          theme_minimal()
+      ) +
+      draw_image(dogeImage)
   })
 }
 
